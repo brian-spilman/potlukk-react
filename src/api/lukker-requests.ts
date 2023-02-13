498ca1256314a9a5a9cb2c401cb9aeb58ac130e5
brian.spilman <brian.spilman@wsu.edu>
2023-02-13 11:26:52 -0800

    basic navigation reducer implementation

--- src/api/lukker-requests.ts
+++ src/api/lukker-requests.ts
@@ -1,14 +1,22 @@
+import { Lukker, PotLukkerRegistrationDetails } from "../reducers/registration-reducer";
 
-export type Lukker = {
-    userId: number,
-    username: string,
-    fname: string,
-    lname: string
-}
 
 export async function getLukkers(): Promise<Lukker[]>{
 
     const httpResponse = await fetch("http://127.0.0.1:8000/lukkers")
     const lukkers: Lukker[] = await httpResponse.json();
     return lukkers;
+}
+
+export async function createLukker(newLukker: PotLukkerRegistrationDetails): Promise<Lukker> {
+    const httpResponse = await fetch("http://127.0.0.1:8000/lukkers", {
+        method:"POST",
+        body:JSON.stringify(newLukker),
+        headers: {
+            "Content-Type":"application/json"
+        }
+    });
+
+    const lukker:Lukker = await httpResponse.json();
+    return lukker;
 }
