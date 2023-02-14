d1f44e451e293a3c10d97ad615e1aa6b07eba801
Bridget Galang <galanbri@gmail.com>
2023-02-14 07:39:14 -0800

    regex-testing

--- src/reducers/registration-reducer.ts
+++ src/reducers/registration-reducer.ts
@@ -27,32 +27,32 @@ export type RegistrationState = {
     isVerified: boolean
 };
 
-export type SetLukkerUsernameAction = {type:"SET_USERNAME", payload: string};
-export type SetLukkerPasswordOneAction = {type:"SET_PASSWORD_ONE", payload: string};
-export type SetLukkerPasswordTwoAction = {type:"SET_PASSWORD_TWO", payload: string};
-export type SetLukkerFnameAction = {type:"SET_FIRST_NAME", payload: string};
-export type SetLukkerLnameAction = {type:"SET_LAST_NAME", payload: string};
-export type SetAllergyAction = {type:"SET_ALLERGY", payload:string};
-export type VerifyLukkerAction = {type:"VERIFY_LUKKER"};
+export type SetLukkerUsernameAction = { type: "SET_USERNAME", payload: string };
+export type SetLukkerPasswordOneAction = { type: "SET_PASSWORD_ONE", payload: string };
+export type SetLukkerPasswordTwoAction = { type: "SET_PASSWORD_TWO", payload: string };
+export type SetLukkerFnameAction = { type: "SET_FIRST_NAME", payload: string };
+export type SetLukkerLnameAction = { type: "SET_LAST_NAME", payload: string };
+export type SetAllergyAction = { type: "SET_ALLERGY", payload: string };
+export type VerifyLukkerAction = { type: "VERIFY_LUKKER" };
 
 export type RegistrationAction = SetLukkerUsernameAction | SetLukkerPasswordOneAction | SetLukkerPasswordTwoAction | SetAllergyAction | SetLukkerFnameAction | SetLukkerLnameAction | VerifyLukkerAction;
 
 const initialState: RegistrationState = {
     username: "",
     password1: "",
-    password2:"",
+    password2: "",
     fname: "",
     lname: "",
     allergies: [],
     isVerified: false
 }
 
-export function registrationReducer(state: RegistrationState = initialState, action: RegistrationAction): RegistrationState{
+export function registrationReducer(state: RegistrationState = initialState, action: RegistrationAction): RegistrationState {
 
     const nextState: RegistrationState = JSON.parse(JSON.stringify(state));
 
-    
-    switch(action.type) {
+
+    switch (action.type) {
 
         case "SET_USERNAME": {
             nextState.username = action.payload;
@@ -76,7 +76,7 @@ export function registrationReducer(state: RegistrationState = initialState, act
         }
         case "SET_ALLERGY": {
             const item = action.payload.toUpperCase();
-            if(nextState.allergies.includes(item)){
+            if (nextState.allergies.includes(item)) {
                 const index = nextState.allergies.indexOf(item);
                 nextState.allergies.splice(index, 1);
                 return nextState;
@@ -91,13 +91,16 @@ export function registrationReducer(state: RegistrationState = initialState, act
 
             //console.log("Right here");
 
-            // const passwordValidation = "?=.*[*.!@$%^&?~\]{9,}"
+            // const passwordValidation = /(?=^.{10,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"/;
+            // const passwordValidation = /[A-Za-z0-9][\!\@\#\$\*]+{10,}/;
+            // // const passwordValidation = /^{10,}(?=.*[A-Za-z])(?=.*[0-9\!\@\#\$\*]+)$/
             // if(!(nextState.password1.match(passwordValidation))){
-            //     alert("Password doesn't include symbols and/or correct length of at least 10")
             //     return nextState;
             // }
 
-            if(nextState.password1 !== nextState.password2){
+            checkRegex(nextState.password1);
+
+            if (nextState.password1 !== nextState.password2) {
                 //alert("Passwords don't match! Fix and try again.");
                 nextState.isVerified = false;
                 return nextState;
@@ -110,6 +113,19 @@ export function registrationReducer(state: RegistrationState = initialState, act
             // figure out how to change from registration page to 
             // navigate("/home");
 
+        }
+
+    }
+
+    function checkRegex(password1: string) {
+        const checkSpecial = /[0-9*@!#%&~]+/.test(state.password1)
+        let valid = false;
+        if (!checkSpecial && nextState.password1.length >= 10) {
+            valid = true;
+            return nextState;
+        } else {
+            valid = false;
+            return nextState;
         }
     }
 }
