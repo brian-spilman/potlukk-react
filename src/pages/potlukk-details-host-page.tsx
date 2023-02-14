58ceeab10325ec6ab1903889cf276102d015b57b
Bridget Galang <galanbri@gmail.com>
2023-02-14 08:48:33 -0800

    added ui and installed datepicker

--- src/pages/potlukk-details-host-page.tsx
+++ src/pages/potlukk-details-host-page.tsx
@@ -1,13 +1,62 @@
+import { NavBar } from "../component/navbar";
+import { useState } from "react"; 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 
 export function PotlukkDetailsHostPage() {
 
 
+    // const date = () => {
+    //     const [startDate, setStartDate] = useState(newDate());
+    //     return (
+    //         <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    //     );
+    // }
 
     return <>
-    
-        <h1>Potlukk Details Host</h1>
-    
+
+    <NavBar />
+
+        {/* <DatePicker selected={startdate} onChange={(date) => setStartDate(date)} />
+
+        <DatePicker
+            selected={date}
+            onChange={handleDateChange}
+            showTimeSelect
+            dateFormat="Pp"
+        /> */}
+
+        <label htmlFor="location">Location</label>
+        <input type="text" />
+
+        <label htmlFor="description">Description</label>
+        <input type="text" />
+
+        <label htmlFor="isPublic">Make Public</label>
+        <input id="isPublic" type="checkbox" />
+
+        <input type="text" placeholder="Search Lukkers" />
+        <button>Search</button>
+
+        <table>
+            <thead><tr>Lukkers</tr></thead>
+            <tbody>
+                <tr>
+
+                </tr>
+            </tbody>
+        </table>
+
+        <table>
+            <thead><tr>Invited Lukkers</tr></thead>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
+
     </>
 }
