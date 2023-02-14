bec7f0b5dd1414e949653be6322a8d1465130ba3
Bridget Galang <galanbri@gmail.com>
2023-02-14 13:29:59 -0800

    date picker testing

--- src/pages/potlukk-details-host-page.tsx
+++ src/pages/potlukk-details-host-page.tsx
@@ -2,31 +2,31 @@ import { NavBar } from "../component/navbar";
 import { useState } from "react"; 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { PotlukkCreationAction } from "../reducers/potlukk-host-reducer";
+import { useDispatch } from "react-redux";
 
 
 
 export function PotlukkDetailsHostPage() {
 
 
-    // const date = () => {
-    //     const [startDate, setStartDate] = useState(newDate());
-    //     return (
-    //         <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-    //     );
-    // }
+    // const dispatch = useDispatch()<PotlukkCreationAction>;
+
+    // const [date, setDate] = useState(null);
 
     return <>
 
     <NavBar />
 
-        {/* <DatePicker selected={startdate} onChange={(date) => setStartDate(date)} />
-
-        <DatePicker
-            selected={date}
-            onChange={handleDateChange}
-            showTimeSelect
-            dateFormat="Pp"
+        {/* <DatePicker
+            selected={selectedDate}
+            onChange={(e) => dispatch({type:"SET_TIME", payload: e.target.value})}
+            dateFormat=
         /> */}
+
+        {/* <DatePicker selected={date} onChange={e => dispatch({type: "SET_TIME", payload: e.target.value})} /> */}
+
+
 
         <label htmlFor="location">Location</label>
         <input type="text" />
@@ -37,26 +37,29 @@ export function PotlukkDetailsHostPage() {
         <label htmlFor="isPublic">Make Public</label>
         <input id="isPublic" type="checkbox" />
 
-        <input type="text" placeholder="Search Lukkers" />
-        <button>Search</button>
+        <button>Update</button>
+        <button>Edit</button>
+        <button>Cancel</button>
 
         <table>
-            <thead><tr>Lukkers</tr></thead>
-            <tbody>
-                <tr>
-
-                </tr>
-            </tbody>
+            <tr><thead><th>Dishes</th></thead></tr>
+            <tr>
+                
+                <td><button>Edit</button></td>
+            </tr>
         </table>
+
+        <button>Bring Dish</button>
+        <button>Request Dish</button>
 
         <table>
-            <thead><tr>Invited Lukkers</tr></thead>
-            <tbody>
-                <tr>
-                    
-                </tr>
-            </tbody>
+            <tr><thead><th>Attendees</th></thead></tr>
+            <tr>
+
+            </tr>
         </table>
+
+        <button>Invite</button>
 
     </>
 }
